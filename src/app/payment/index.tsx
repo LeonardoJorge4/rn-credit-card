f868b78bec971cfbf3c8def042dcbe3cfef1a5a3
Leonardo Jorge <leonardoti4437@gmail.com>
2024-04-17 22:31:43 -0300

    feat: create project with card animation

---
+++ src/app/payment/index.tsx
@@ -0,0 +1,37 @@
+import { Text, TouchableOpacity, View } from 'react-native';
+import { useSharedValue } from 'react-native-reanimated';
+import { CARD_SIDE, CreditCard } from '@/components/credit-card';
+import { styles } from './styles';
+
+export function Payment() {
+  const cardSide = useSharedValue(CARD_SIDE.front);
+
+  function showFrontCard() {
+    cardSide.value = CARD_SIDE.front;
+  }
+
+  function showBackCard() {
+    cardSide.value = CARD_SIDE.back;
+  }
+
+  function handleFlipCard() {
+    if (cardSide.value === CARD_SIDE.front) {
+      showBackCard();
+    } else {
+      showFrontCard();
+    }
+  }
+
+  return (
+    <View style={styles.container}>
+      <CreditCard cardSide={cardSide} />
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={handleFlipCard}
+      >
+        <Text>Inverter</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
---
+++ src/components/credit-card/index.tsx
@@ -0,0 +1,86 @@
+import { Text, View } from 'react-native';
+import { styles } from './styles';
+import Animated, {
+  SharedValue,
+  interpolate,
+  useAnimatedStyle,
+  withTiming,
+} from 'react-native-reanimated';
+
+type CreditCardProps = {
+  cardSide: SharedValue<number>;
+};
+
+export enum CARD_SIDE {
+  front = 0,
+  back = 1,
+}
+
+export function CreditCard({ cardSide }: CreditCardProps) {
+  const frontAnimatedStyles = useAnimatedStyle(() => {
+    const rotateValue = interpolate(
+      cardSide.value,
+      [CARD_SIDE.front, CARD_SIDE.back],
+      [0, 180]
+    );
+
+    return {
+      transform: [
+        { rotateY: withTiming(`${rotateValue}deg`, { duration: 1000 }) },
+      ],
+    };
+  });
+
+  const backAnimatedStyles = useAnimatedStyle(() => {
+    const rotateValue = interpolate(
+      cardSide.value,
+      [CARD_SIDE.front, CARD_SIDE.back],
+      [180, 360]
+    );
+
+    return {
+      transform: [
+        { rotateY: withTiming(`${rotateValue}deg`, { duration: 1000 }) },
+      ],
+    };
+  });
+
+  return (
+    <View>
+      <Animated.View style={[styles.card, styles.front, frontAnimatedStyles]}>
+        <View style={styles.header}>
+          <View style={[styles.circle, styles.logo]} />
+          <Text>Meu Cartão</Text>
+        </View>
+
+        <View style={styles.footer}>
+          <Text style={styles.name}>Leonardo Jorge</Text>
+
+          <View style={styles.flag}>
+            <View style={[styles.circle, styles.red]} />
+            <View style={[styles.circle, styles.orange]} />
+          </View>
+        </View>
+      </Animated.View>
+
+      <Animated.View style={[styles.card, styles.back, backAnimatedStyles]}>
+        <View>
+          <Text style={styles.label}>Número do cartão</Text>
+          <Text style={styles.value}>1234 5678 9012 3456</Text>
+        </View>
+
+        <View style={styles.footer}>
+          <View>
+            <Text style={styles.label}>Validade</Text>
+            <Text style={styles.value}>12/02</Text>
+          </View>
+
+          <View>
+            <Text style={styles.label}>CVV</Text>
+            <Text style={styles.value}>123</Text>
+          </View>
+        </View>
+      </Animated.View>
+    </View>
+  );
+}
